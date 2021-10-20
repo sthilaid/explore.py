6ba6821958b3323a94e588ab2dcd22d891480ae6
David St-Hilaire <sthilaire.david@gmail.com>
2021-10-19 22:30:48 -0400

    added more info and sorting

--- explore.py
+++ explore.py
@@ -3,11 +3,26 @@
 from curses import wrapper
 import os, subprocess, shutil, curses, time, pickle, sys
 
+sortfns = [lambda x: x.filename,
+               lambda x: x.timestamp,
+               lambda x: x.filesize,
+               lambda x: x.rating,
+               lambda x: x.star,
+               ]
+sortkey = 0
+sortdir = 0
+max_file_length = 0
+max_stamp_length = 0
+max_fsize_length = 0
+max_star_length = 0
+max_rating_length = 0
+
 class File:
-    def __init__(self, filename, ext, timestamp):
+    def __init__(self, filename, ext, timestamp, fsize):
         self.filename = filename
         self.ext = ext
         self.timestamp = timestamp
+        self.filesize = fsize
         self.star = ""
         self.rating = 0
 
@@ -29,7 +44,6 @@ def syncFilesDB(files, filesdb, fromDB):
             updated_file.star = db_file.star
             updated_file.rating = db_file.rating
         else:
-            dbfiles[i].timestamp = updated_file.timestamp
             dbfiles[i].star = updated_file.star
             dbfiles[i].rating = updated_file.rating
             
@@ -51,29 +65,55 @@ def get_files(path, desired_ext):
         (file,ext) = os.path.splitext(f)
         if ext == desired_ext:
             stamp = os.path.getmtime(path+"/"+f)
-            files.append(File(file, ext, stamp))
+            fsize = os.path.getsize(path+"/"+f)
+            files.append(File(file, ext, stamp, fsize))
     return files
 
-def draw_title(pad, max_file_length, max_stamp_length, max_star_length):
-    pad.addstr(0, 0, "filename", curses.A_BOLD)
-    pad.addstr(0, max_file_length+1, "last modif", curses.A_BOLD)
-    pad.addstr(0, max_file_length+1+max_stamp_length+1, "star", curses.A_BOLD)
-    star_space = max(0,max_star_length+1-4)
-    pad.addstr(0, max_file_length+1+max_stamp_length+5, " " * star_space, curses.A_BOLD)
-    pad.addstr(0, max_file_length+1+max_stamp_length+1+max_star_length+1, "rating", curses.A_BOLD)
+def draw_title(pad):
+    global max_file_length, max_stamp_length, max_fsize_length, max_rating_length, max_star_length, sortkey, sortdir
+
+    msg = "filename" + ("▲" if (sortkey == 0 and sortdir == 0) else ("▼" if (sortkey == 0 and sortdir == 1) else " "))
+    pad.addstr(0, 0, msg, curses.A_BOLD)
+
+    offset=max_file_length+1
+    msg = "last modif" + ("▲" if (sortkey == 1 and sortdir == 0) else ("▼" if (sortkey == 1 and sortdir == 1) else " "))
+    pad.addstr(0, offset, msg, curses.A_BOLD)
+    
+    offset += max_stamp_length+1
+    msg = "size" + ("▲" if (sortkey == 2 and sortdir == 0) else ("▼" if (sortkey == 2 and sortdir == 1) else " "))
+    pad.addstr(0, offset, msg, curses.A_BOLD)
+    
+    offset += max_fsize_length+1
+    msg = "rating" + ("▲" if (sortkey == 3 and sortdir == 0) else ("▼" if (sortkey == 3 and sortdir == 1) else " "))
+    pad.addstr(0, offset, msg, curses.A_BOLD)
+    
+    offset += max_rating_length+1
+    msg = "star" + ("▲" if (sortkey == 4 and sortdir == 0) else ("▼" if (sortkey == 4 and sortdir == 1) else " "))
+    pad.addstr(0, offset, msg, curses.A_BOLD)
+    pad.addstr(0, offset+5, " " * 60, curses.A_BOLD)
     pad.addstr(1, 0, "=" * 100, curses.A_BOLD)
 
-def draw_files(pad, files, selection, start_y, max_file_length, max_star_length, max_stamp_length):
+def draw_files(pad, files, selection, start_y):
+    global max_file_length, max_star_length, max_rating_length, max_stamp_length, max_fsize_length
     lineIndex = 0
     for i,f in enumerate(files):
         attr = curses.A_STANDOUT if i == selection else 0
         pad.addstr(start_y+i, 0, f.filename, attr)
+        pad.addstr(start_y+i, len(f.filename), " "*(max_file_length - len(f.filename)), attr)
         stampstr = time.asctime(time.localtime(f.timestamp))
-        pad.addstr(start_y+i, max_file_length+1, stampstr, attr)
-        pad.addstr(start_y+i, max_file_length+1+max_stamp_length+1, f.star, attr)
-        pad.addstr(start_y+i, max_file_length+1+max_stamp_length+1+max_star_length+1, "*" * f.rating + " " * (5 - f.rating), attr)
+        xoffset = max_file_length+1
+        pad.addstr(start_y+i, xoffset, stampstr, attr)
+        xoffset += max_stamp_length+1
+        pad.addstr(start_y+i, xoffset, '{:.2f} GB'.format(f.filesize*0.000000001), attr);
+        xoffset += max_fsize_length+1
+        pad.addstr(start_y+i, xoffset, "*" * f.rating + " " * (5 - f.rating), attr)
+        xoffset += max_rating_length+1
+        pad.addstr(start_y+i, xoffset, f.star, attr)
+        pad.addstr(start_y+i, xoffset+len(f.star), " "*40, attr)
 
 def main(stdscr):
+    global sortkey, sortdir, max_file_length, max_stamp_length, max_fsize_length, max_star_length, max_rating_length
+    
     curses.curs_set(0) # hide cursor
     screen = curses.initscr()
     pad = curses.newpad(100,100)
@@ -94,6 +134,7 @@ def main(stdscr):
 
     files = get_files(rootFolder, extensionFilter)
     syncFilesDB(files, rootFolder+"/"+filesdb, True)
+    files.sort(key=sortfns[sortkey], reverse=(sortdir==1))
 
     if len(files) == 0:
         return
@@ -101,16 +142,18 @@ def main(stdscr):
     selection=0
     max_file_length = max(len(f.filename) for f in files)
     max_stamp_length = len(time.asctime())
+    max_fsize_length = 8
     max_star_length = max(len("star"), max(len(f.star) for f in files))
+    max_rating_length = 7
 
-    draw_title(pad, max_file_length, max_stamp_length, max_star_length)
+    draw_title(pad)
 
     header= 2
     max_y = 22
     files_max_y = max_y - header
     
     while(True):
-        draw_files(pad, files, selection, header, max_file_length, max_star_length, max_stamp_length)
+        draw_files(pad, files, selection, header)
         pad_y = selection - files_max_y if selection > files_max_y else 0
         pad.refresh(pad_y, 0, 0, 0, max_y, 79)
 
@@ -126,13 +169,22 @@ def main(stdscr):
             files[selection].rating = min(5, files[selection].rating+1)
             syncFilesDB(files, rootFolder+"/"+filesdb, False)
         elif key == "e":
-            files[selection].star = pad.getstr()
+            input = pad.getstr().decode("utf-8")
+            files[selection].star = input if input else ""
             syncFilesDB(files, rootFolder+"/"+filesdb, False)
             max_star_length = max(len("star"), max(len(f.star) for f in files))
-            draw_title(pad, max_file_length, max_stamp_length, max_star_length)
+            draw_title(pad)
         elif key == "\n":
             selectedFile = rootFolder+"/"+files[selection].filename + files[selection].ext
             subprocess.run([cmdProcess, f'{os.path.abspath(selectedFile)}'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        elif key == "s":
+            if sortdir == 0:
+                sortdir = 1
+            else:
+                sortdir = 0
+                sortkey = (sortkey +1) % len(sortfns)
+            files.sort(key=sortfns[sortkey], reverse=(sortdir==1))
+            draw_title(pad)
         elif key == "q" or key == "Q":
             exit()
 
